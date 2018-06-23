be58ddbbfa82ba8b21ef832fd1fa93d9281cc0bf
mirok <miroslav.kuchta@gmail.com>
2018-06-23 21:20:39 +0200

    mean and rms of series

--- xcalc/operators.py
+++ xcalc/operators.py
@@ -45,6 +45,44 @@ def Eigv(expr):
                         op=lambda A: np.linalg.eig(A)[1].T,
                         shape_res=(n, m))
 
+def Mean(series):
+    '''A mean of the series is 1/(T - t0)\int_{t0}^{t1}f(t)dt'''
+    # Apply simpsons rule
+    mean = Function(series.V)
+    x = mean.vector()
+    # NOTE: for effiecency we stay away from Interpreter
+    # Int
+    dts = np.diff(series.times)
+    for dt, (f0, f1) in zip(dts, zip(series.functions[:-1], series.functions[1:])):
+        x.axpy(dt/2, f0.vector())  # (f0+f1)*dt/2
+        x.axpy(dt/2, f1.vector())
+    # Time interval scaling
+    x /= dts.sum()
+
+    return mean
+
+
+def RMS(series):
+    '''sqrt(1/(T - t0)\int_{t0}^{t1}f^2(t)dt'''
+    # Again by applying simpson
+    rms = Function(series.V)
+    # NOTE: for efficiency we stay away from Interpreter and all is in PETSc layer
+    x = as_backend_type(rms.vector()).vec()  # PETSc.Vec
+    y = x.copy()  # Stores fi**2
+    # Integrate
+    dts = np.diff(series.times)
+    f_vectors = [as_backend_type(f.vector()).vec() for f in series.functions]
+    for dt, (f0, f1) in zip(dts, zip(f_vectors[:-1], f_vectors[1:])):
+        y.pointwiseMult(f0, f0)  # y = f0**2
+        x.axpy(dt/2, y)  # (f0**2+f1**2)*dt/2
+
+        y.pointwiseMult(f1, f1)  # y = f1**2
+        x.axpy(dt/2, y)
+    # Time interval scaling
+    x /= dts.sum()
+
+    return rms
+    
 # ------------------------------------------------------------------------------
 
 if __name__ == '__main__':
@@ -58,7 +96,6 @@ if __name__ == '__main__':
 
     print Eigw(f+f)(0.5, 0.5)    
     print np.linalg.eigvals(A+A)
-
 
     g = Eigv(f+f)
     print g(0.5, 0.5)    
